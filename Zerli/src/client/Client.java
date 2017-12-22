ad12bb557e9377462147f534aa2a3cd6334c0a99
danik <danik@LAPTOP-SLBNBS2K>
2017-12-22 18:16:13 +0200

    saf

--- Zerli/src/client/Client.java
+++ Zerli/src/client/Client.java
@@ -7,7 +7,6 @@ public class Client implements ConIF {
 	/*
 	 * Client is connecting the client to the server
 	 */
-	//sex
 	final public static int DEFAULT_PORT = 5551;
 	public static ClientController clientConn;
 	public static String host = "localhost"; 
