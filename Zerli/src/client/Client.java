da199a71fb997f073cab169f1588d86779f16a92
danik <danik@LAPTOP-SLBNBS2K>
2017-12-22 18:28:19 +0200

    asf

--- Zerli/src/client/Client.java
+++ Zerli/src/client/Client.java
@@ -15,7 +15,7 @@ public class Client implements ConIF {
 
 	// setup client connection
 	public Client(String host, int port) {
-		System.out.println("Gayxxx");
+		System.out.println("lior ata cocksinal!!! ");
 		try {
 			clientConn = new ClientController(Client.host, Client.DEFAULT_PORT, this);
 			
