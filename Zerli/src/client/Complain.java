1ff47c50807b366c6a04def0edcc02fbb4cf8df8
liorxO <34256418+liorxO@users.noreply.github.com>
2017-12-29 16:53:58 +0200

    Add files via upload

--- Zerli/src/client/Complain.java
+++ Zerli/src/client/Complain.java
@@ -3,19 +3,35 @@ package client;
 import java.io.Serializable;
 
 public class Complain implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private int complainId;
 	private User user;
+	private int userID;
 	private String desc;
-	private int isAnswered;
+	private String status;
 	private float compensation;
-	public Complain (int complainId, User user, String desc, int isAnswered, float compensation)
+	public Complain (int complainId, User user, String desc, String status, float compensation)
 	{
-		this.setAnswered(isAnswered);
+		this.setStatus(status);
 		this.setCompensation(compensation);
 		this.setComplainId(complainId);
 		this.setDesc(desc);
 		this.setUser(user);
+		this.setUserID(this.user.getId());
 	}	
+	
+	public Complain (int complainId, int userID, String desc, String status, float compensation)
+	{
+		this.setStatus(status);
+		this.setCompensation(compensation);
+		this.setComplainId(complainId);
+		this.setDesc(desc);
+		this.setUserID(this.user.getId());
+	}	
+	
 	
 	public int getComplainId() {
 		return complainId;
@@ -35,17 +51,27 @@ public class Complain implements Serializable {
 	public void setDesc(String desc) {
 		this.desc = desc;
 	}
-	public int getisAnswered() {
-		return isAnswered;
-	}
-	public void setAnswered(int isAnswered) {
-		this.isAnswered = isAnswered;
-	}
 	public float getCompensation() {
 		return compensation;
 	}
 	public void setCompensation(float compensation) {
 		this.compensation = compensation;
+	}
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
 	}
 	
 }
